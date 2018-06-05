2b43aec689cfb3246bce499839d37dad6db3449e
Kalle Laakso <laakso29@luukku.com>
2018-06-05 13:40:11 +0300

    Statistics button added for testing WorkoutHistory activity

---
+++ app/src/main/java/com/finn/laakso/hangboardapp/WorkoutStatistics.java
@@ -0,0 +1,17 @@
+package com.finn.laakso.hangboardapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class WorkoutStatistics extends AppCompatActivity {
+
+    //ListView workoutHistoryListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_statistics);
+
+       // workoutHistoryListView = (ListView) findViewById(R.id.workoutHistoryListView);
+    }
+}
