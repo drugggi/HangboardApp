9ce9b9499fc32827f9d23269d7a4a6c55cf8d0aa
Kalle Laakso <laakso29@luukku.com>
2018-06-05 15:42:53 +0300

    ListView and ScrollView added to WorkoutStatistics activity.

--- app/src/main/java/com/finn/laakso/hangboardapp/WorkoutStatistics.java
+++ app/src/main/java/com/finn/laakso/hangboardapp/WorkoutStatistics.java
@@ -2,16 +2,23 @@ package com.finn.laakso.hangboardapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class WorkoutStatistics extends AppCompatActivity {
 
-    //ListView workoutHistoryListView;
+    ListView workoutHistoryListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_statistics);
 
-       // workoutHistoryListView = (ListView) findViewById(R.id.workoutHistoryListView);
+        HangBoard testBoard = new HangBoard(getResources());
+
+        workoutHistoryListView = (ListView) findViewById(R.id.workoutHistoryListView);
+        final ArrayAdapter<String> gradeAdapter = new ArrayAdapter<String>(this, R.layout.gradetextview, testBoard.getGrades());
+        workoutHistoryListView.setAdapter(gradeAdapter);
+
     }
 }
