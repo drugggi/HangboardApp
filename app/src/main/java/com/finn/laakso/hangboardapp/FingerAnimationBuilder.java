00df574bf1cb8acf11e1662121be912a9d416ef9
Kalle Laakso <laakso29@luukku.com>
2018-10-30 09:46:45 +0200

    Added finger animation 3f to 2b and back.

--- app/src/main/java/com/finn/laakso/hangboardapp/FingerAnimationBuilder.java
+++ app/src/main/java/com/finn/laakso/hangboardapp/FingerAnimationBuilder.java
@@ -6,7 +6,7 @@ public final class FingerAnimationBuilder {
             0,R.drawable.animation_left_4_to_3f,R.drawable.animation_left_4_to_3b,R.drawable.animation_left_4_to_2f,R.drawable.animation_left_4_to_2m,R.drawable.animation_left_4_to_2b};
 
     private static int[] leftHand_3f_to_x = {
-            R.drawable.animation_left_3f_to_4,0,R.drawable.animation_left_3f_to_3b,R.drawable.animation_left_3f_to_2f,R.drawable.animation_left_3f_to_2m,0};
+            R.drawable.animation_left_3f_to_4,0,R.drawable.animation_left_3f_to_3b,R.drawable.animation_left_3f_to_2f,R.drawable.animation_left_3f_to_2m,R.drawable.animation_left_3f_to_2b};
 
     private static int[] leftHand_3b_to_x = {
             R.drawable.animation_left_3b_to_4,R.drawable.animation_left_3b_to_3f,0,R.drawable.animation_left_3b_to_2f,R.drawable.animation_left_3b_to_2m,0};
@@ -15,7 +15,7 @@ public final class FingerAnimationBuilder {
 
     private static int[] leftHand_2m_to_x = {R.drawable.animation_left_2m_to_4,R.drawable.animation_left_2m_to_3f,R.drawable.animation_left_2m_to_3b,R.drawable.animation_left_2m_to_2f,0,0};
 
-    private static int[] leftHand_2b_to_x = {R.drawable.animation_left_2b_to_4,0,0,0,0,0};
+    private static int[] leftHand_2b_to_x = {R.drawable.animation_left_2b_to_4,R.drawable.animation_left_2b_to_3f,0,0,0,0};
 
 
 
@@ -23,7 +23,7 @@ public final class FingerAnimationBuilder {
             0,R.drawable.animation_right_4_to_3f,R.drawable.animation_right_4_to_3b,R.drawable.animation_right_4_to_2f,R.drawable.animation_right_4_to_2m,R.drawable.animation_right_4_to_2b};
 
     private static int[] rightHand_3f_to_x = {
-            R.drawable.animation_right_3f_to_4,0,R.drawable.animation_right_3f_to_3b,R.drawable.animation_right_3f_to_2f,R.drawable.animation_right_3f_to_2m,0};
+            R.drawable.animation_right_3f_to_4,0,R.drawable.animation_right_3f_to_3b,R.drawable.animation_right_3f_to_2f,R.drawable.animation_right_3f_to_2m,R.drawable.animation_right_3f_to_2b};
 
     private static int[] rightHand_3b_to_x = {
             R.drawable.animation_right_3b_to_4,R.drawable.animation_right_3b_to_3f,0,R.drawable.animation_right_3b_to_2f,R.drawable.animation_right_3b_to_2m,0};
@@ -32,7 +32,7 @@ public final class FingerAnimationBuilder {
 
     private static int[] rightHand_2m_to_x = {R.drawable.animation_right_2m_to_4,R.drawable.animation_right_2m_to_3f,R.drawable.animation_right_2m_to_3b,R.drawable.animation_right_2m_to_2f,0,0};
 
-    private static int[] rightHand_2b_to_x = {R.drawable.animation_right_2b_to_4,0,0,0,0,0};
+    private static int[] rightHand_2b_to_x = {R.drawable.animation_right_2b_to_4,R.drawable.animation_right_2b_to_3f,0,0,0,0};
 
 
 
